55a9163d52ca9a258d8f82cf056366b775546a0c
here_io <onedupark@outlook.com>
2023-05-12 00:41:34 +0900

    feat: Rigidbody 와 프레임에 입력되는 값과 상호작용하는 메시지함수를 담은 스크립트 구현

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    public GameObject player;
+    private Vector3 offset; 
     // Start is called before the first frame update
     void Start()
+    {
+        //Given 해당 카메라의 position 값과, player의 position값이 있다고 전제를 하는 기능이 있기에, Awake보다는 Start가 적절하다. 
+
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update 는 비록 새로운 프레임 이전에 호출되지만, Delegate += function 처럼 순서를 보장하지는 못한다. 
+    // 특히 Camera offset 값에 따라 카메라의 위치가 결정될때, 일반적인 물리연산함수이후에 Update()가 되어야 하는데, 이를 보장하지 못한다. 
+    // 따라서 해당scene 의 모든 Game object 의 메세지함수중, Update() 가 다 끝난 이후에 진행되는 FixedUpdate()를 사용하는것이 일반적이겠다. 
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void LateUpdate()
     {
-        
+       // 같은 맥락으로 다른 함수값을 이용해야하는경우에도 lateUpdate()가 필요하겠다. 
+       transform.position = offset + player.transform.position;
     }
 }
--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public class TankController : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Rigidbody rigidbody; // 물리적 연산이 적용가능한 유니티 data type 이자 GameObject 에 추가 가능한 Component
+    [Range(0, 50)]
+    public int jumpForce; 
 
     void Start()
     {
-        
+        // Rigidbody 가 생성되었으며, 해당 components 이 gameobj의 rigidbody 컴포넌트를 이미 콜링하고 있다고 가정한다 
+        rigidbody.AddForce(Vector3.up*(jumpForce*2), ForceMode.Impulse);
+        Debug.Log("생성되서 기뻐 점프합니다 탱크가"); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space)) // 이전프레임에 입력받은 값을 전달하며, 만약 KeyCode.Space가 맞다면 true 반환 
+        {
+            rigidbody.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
+            Debug.Log("스페이스키 입력");
+        }
+    }
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>(); // 이미 해당 gameObject 에 rigidbody를 생성함과 동시에 rigidbody 와 상호작용을 일으킬 함수를 만들기
+                                               // 위한 필수조건인, 
+        jumpForce = 10; 
     }
 }
